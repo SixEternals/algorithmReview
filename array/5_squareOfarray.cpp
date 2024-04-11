4cf6831badbc85b0188eebf9c78bcd441060324b
jun2 <baishun8181@163.com>
2024-04-11 13:33:59 +0800

    5 6

---
+++ array/5_squareOfarray.cpp
@@ -0,0 +1,62 @@
+// https://leetcode.cn/problems/squares-of-a-sorted-array/description/
+/*
+977. 有序数组的平方
+给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+
+示例 1：
+输入：nums = [-4,-1,0,3,10]
+输出：[0,1,9,16,100]
+解释：平方后，数组变为 [16,1,0,9,100]
+排序后，数组变为 [0,1,9,16,100]
+
+示例 2：
+输入：nums = [-7,-3,2,3,11]
+输出：[4,9,9,49,121]
+
+提示：
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums 已按 非递减顺序 排序
+*/
+/*
+无敌了 这也能双指针
+  s        f        s      f             s   f               f
+[-7,-3,2,3,11] -> [-7,-3,2,3,11] -> [-7,-3,2,3,11] -> [-7,-3,2,3,11]
+new[11^2, -7^2, -3^2, 3^2, 2^2]
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    int index;
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        vector<int> arr(nums.size());
+        index = nums.size() - 1;
+        int slow = 0, fast = nums.size() - 1;
+        while (slow <= fast)
+        {
+            if (abs(nums[slow]) <= abs(nums[fast]))
+            {
+                arr[index--] = nums[fast] * nums[fast];
+                fast--;
+            }
+            else
+            {
+                arr[index--] = nums[slow] * nums[slow];
+                slow++;
+            }
+        }
+        return arr;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution instance;
+    vector<int> nums{-7, -3, 2, 3, 11};
+    instance.sortedSquares(nums);
+    return 0;
+}
